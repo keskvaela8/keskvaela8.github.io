edf8dd9806e18573a8d3c9336be9c60d76d0eff1
Martin Sooväli <msoovali@gmail.com>
2026-01-06 20:52:41 +0200

    Fix month selection

--- app/util/selectablePeriods.ts
+++ app/util/selectablePeriods.ts
@@ -1,11 +1,11 @@
 export const selectablePeriods = () => {
   const periods: string[] = [];
   const now = new Date();
-  for (let i = 0; i < 6; i++) {
-    const year = now.getFullYear();
-    const month = String(now.getMonth()).padStart(2, "0");
-    periods.push(`${year}_${month}`);
+  for (let i = 0; i < 12; i++) {
     now.setMonth(now.getMonth() - 1);
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    periods.push(`${year}_${month}`);
   }
   return periods;
 };
